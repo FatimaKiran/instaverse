56f49862bfc92a0d825553a0ac861fb699198ae5
FatimaKiran <fmian7860@gmail.com>
2022-06-13 22:21:03 +0100

    like functionality added

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -40,4 +40,13 @@ export const createPost = (post) => async (dispatch) => {
     }catch(error){
         console.log(error);
     }
+ };
+
+ export const likePost = (id) => async(dispatch) =>{
+    try{
+        const { data } = await api.likePost(id);
+        dispatch({type: 'LIKE', payload: data});
+    }catch(err){
+        console.log(err);
+    }
  }
